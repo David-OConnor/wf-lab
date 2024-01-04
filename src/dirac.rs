93deee9135b76c6f2868c8026ee531ae2b5cf305
David O'Connor <the_alchemist@fastmail.com>
2024-01-03 20:05:41 -0500

    More work on Dirac

--- src/dirac.rs
+++ src/dirac.rs
@@ -1,6 +1,10 @@
 //! Scratchpad for Dirac-equation implementation.
+//! Intent: See if this clarifies how to handle spin.
 //!
 //! Relation of E and p for any particle: E^2 - phat^2 c^c = m^2 c^4
+//!
+//! α_1^2 = α_2^2 = α_3^2 = β^2 = 1
+//! α and β are 4x4 matrices. α = [0,σ, σ, 0]  β = [1, 0, 0, -1]
 
 use na::{Matrix2, Matrix4};
 use nalgebra as na;
@@ -86,6 +90,35 @@ fn a() {
     let sigma3: Matrix2<f32> = Matrix2::new(
         1., 0.,
         0., -1.,
+    );
+
+    let alpha1: Matrix4<f32> = Matrix4::new(
+        0., 0., sigma1.m11, sigma1.m12,
+        0., 0., sigma1.m21, sigma1.m22,
+        sigma1.m11, sigma1.m12, 0., 0.,
+        sigma1.m21, sigma1.m22, 0., 0.
+    );
+
+    let alpha2: Matrix4<Cplx> = Matrix4::new(
+        C0, C0, sigma2.m11, sigma2.m12,
+        C0, C0, sigma2.m21, sigma2.m22,
+        sigma2.m11, sigma2.m12, C0, C0,
+        sigma2.m21, sigma2.m22, C0, C0
+    );
+
+    let alpha3: Matrix4<f32> = Matrix4::new(
+        0., 0., sigma3.m11, sigma3.m12,
+        0., 0., sigma3.m21, sigma3.m22,
+        sigma3.m11, sigma3.m12, 0., 0.,
+        sigma3.m21, sigma3.m22, 0., 0.
+    );
+    
+    // etc for alpha 2 and 3.
+    let beta: Matrix4<f32> = Matrix4::new(
+        1., 0., 0., 0.,
+        0., 1., 0., 0.,
+        0., 0., -1., 0.,
+        0., 0., 0., -1.
     );
 
 
